70a58eab10b241f19245ee1794047048c5c0b278
chandan maurya <76623223+cr3992@users.noreply.github.com>
2022-05-13 00:50:48 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,10 @@
+function view_function(){
+           var name =  document.getElementById('name').value;
+		   var mobile =  document.getElementById('mobile').value;
+		   var email =  document.getElementById('email').value;
+		   var subject =  document.getElementById('sub').value;
+		   var msg =  document.getElementById('msg').value;
+		   
+		   alert("Name: "+name+"\n"+"Mobile: "+mobile+"\n"+"Email: "+email+"\n"+"Subject: "+subject+"\n"+"Message: "+msg);
+           
+        }
